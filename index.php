c0ac89b36bf813b7119c3ad0eb56804198d9401a
Helior <65434511+X0Fury@users.noreply.github.com>
2021-04-07 23:11:43 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
-function crawl_page($url,$depth=2)
+function crawlpaginarkkkkk($url,$naosei=2)
 {
 
-       if ($depth>0)
+       if ($naosei>0)
        {
-           $html - file_get_contents($url)
-           preg_match_all('~<a.*?href="(.*?)".*?>~', $html,$matches)
+           $conteudodapag - file_get_contents($url)
+           preg_match_all('~<a.*?href="(.*?)".*?>~', $conteudopag,$matches)
            foreach($matches[1] as $novaurl)
        {
-           crawl_page($novaurl,$depth-1);
+           crawl_page($novaurl,$naosei-1);
        }  
-       file_put_contents('results.html', "\n\n".$html."\n\n",FILE_APPEND);
+       // CASO QUEIRA ADICIONAR O HTML EM UM ARQUIVO -> file_put_contents('arquivoaqui', "\n\n".$conteudodapag."\n\n",FILE_APPEND);
     } 
 }
-crawl_page('https://www.tracoactual.com/',2)
+crawlpaginar('https://www.tracoactual.com/',2)
