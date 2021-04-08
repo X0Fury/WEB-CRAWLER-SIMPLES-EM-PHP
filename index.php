d2c79d26d6600cc71f5cd41dc68657e9f8408413
Helior <65434511+X0Fury@users.noreply.github.com>
2021-04-07 23:18:30 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,8 @@
 <?php
-function crawlpaginarkkkkk($url,$naosei=2)
-{
-
-       if ($naosei>0)
-       {
-           $conteudodapag - file_get_contents($url)
-           preg_match_all('~<a.*?href="(.*?)".*?>~', $conteudopag,$matches)
-           foreach($matches[1] as $novaurl)
-       {
-           crawl_page($novaurl,$naosei-1);
-       }  
-       // CASO QUEIRA ADICIONAR O HTML EM UM ARQUIVO -> file_put_contents('arquivoaqui', "\n\n".$conteudodapag."\n\n",FILE_APPEND);
-    } 
+function crawlpaginar($url) {
+       $html_da_pag = file_get_contets($url);
+       preg_match('<a href=', $html_da_pag, $matches, PREG_OFFSET_CAPTURE);
+       print_r($matches);
 }
-crawlpaginar('https://www.tracoactual.com/',2)
+
+crawlpaginar('https://www.github.com');
